3ed70ecc434aa8b877ec3a5a31ac929300fb6127
Alexis M <iamsnkh@pm.me>
2019-11-11 21:46:30 +0100

    :construction: wip: leaks fixed but invalid read & write on multi fd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/03 16:36:36 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/10 07:38:32 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/11 21:42:52 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,23 +18,101 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int		ft_pretty(char **line, t_gnl *current)
+char	*ft_strdup(const char *s)
+{
+	int		count;
+	char	*tab;
+
+	count = 0;
+	while (s[count])
+		count++;
+	if (!(tab = (char *)malloc((count + 1) * sizeof(char))))
+		return (NULL);
+	count = -1;
+	while (s[++count])
+		tab[count] = s[count];
+	tab[count] = '\0';
+	return (tab);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	count;
+	size_t	size;
+	char	*tab;
+
+	count = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	while (count < len)
+	{
+		tab[count] = s[start + count];
+		count++;
+	}
+	tab[count] = '\0';
+	return (tab);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	count;
+	size_t	s1_size;
+	char	*tab;
+
+	count = -1;
+	s1_size = ft_strlen(s1);
+	if (!(tab = (char *)malloc((s1_size + ft_strlen(s2) + 1) * sizeof(char))))
+		return (NULL);
+	while (s1 && s1[++count])
+		tab[count] = s1[count];
+	count = -1;
+	while (s2 && s2[++count])
+		tab[s1_size + count] = s2[count];
+	tab[s1_size + count] = '\0';
+	free(s1);
+	return (tab);
+}
+
+void	ft_strdel(char **str)
+{
+	if (!str || !*str)
+		return ;
+	free(*str);
+	*str = NULL;
+}
+
+int		ft_pretty(char **line, t_gnl *current, t_gnl **first)
 {
 	int		size;
+	t_gnl	*linked;
 
+	linked = NULL;
+	if (first)
+		linked = *first;
 	if (!ft_strchr(current->content, '\n'))
 	{
 		*line = ft_strdup(current->content);
+		ft_strdel(&current->content);
+		while (linked->next && linked->next->fd != current->fd)
+			linked = linked->next;
+		linked->next = current->next;
+		free(current);
 		return (0);
 	}
 	else
 	{
 		size = (int)(ft_strchr(current->content, '\n') - current->content);
 		*line = ft_substr(current->content, 0, size);
-		current->content = ft_strdup(current->content + size + 1);
+		current->content = ft_strjoin(current->content + size + 1, NULL);
 		return (1);
 	}
-	// free(current);
 }
 
 int		get_next_line(int fd, char **line)
@@ -42,63 +120,23 @@ int		get_next_line(int fd, char **line)
 	static t_gnl	*lst;
 	t_gnl			*tmp;
 	char			buffer[BUFFER_SIZE + 1];
+	char			*temp;
 	int				readed;
 
 	readed = 0;
 	if (!line || BUFFER_SIZE < 1 || read(fd, buffer, 0) < 0)
 		return (-1);
 	if (!lst && !(lst = ft_create_list(fd)))
-			return (-1);
-	if (!(tmp = ft_lstchr(lst, fd)) && !(tmp = ft_lstadd(&lst, ft_create_list(fd)))) // si il trouve pas il le creer
 		return (-1);
-	while (!ft_strchr(tmp->content, '\n') && (readed = read(fd, buffer, BUFFER_SIZE)) > 0)
+	if (!(tmp = ft_lstchr(lst, fd)) &&
+		!(tmp = ft_lstadd(&lst, ft_create_list(fd))))
+		return (-1);
+	while (!ft_strchr(tmp->content, '\n')
+	&& (readed = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[readed] = '\0';
-		tmp->content = ft_strjoin(tmp->content, buffer);
+		temp = ft_strjoin(tmp->content, buffer);
+		tmp->content = ft_strjoin(temp, NULL);
 	}
-	return (ft_pretty(line, tmp));
+	return (ft_pretty(line, tmp, &lst));
 }
-
-int		main()
-{
-	char	*line;
-	int 	fd;
-	int 	fd2;
-	int 	i = 0;
-
-	fd = open("a", O_RDONLY);
-	fd2 = open("Makefile", O_RDONLY);
-
-	// fd = open("get_next_line.c", O_RDONLY);
-	// printf("GNL with fd=%d\n", fd);
-	// get_next_line(fd, &line);
-	// printf("<\n%s\n>", line);
-
-
-	while ((i = get_next_line(fd, &line)) > 0)
-	{
-		printf("[%d]\033[32m %s\n", i, line);
-		free(line);
-		line = NULL;
-	}
-	free(line);
-	i = get_next_line(fd, &line);
-	printf("[%d]\033[32m %s\n", i, line);
-
-	while ((i = get_next_line(fd2, &line)) > 0)
-	{
-		printf("[%d]\033[32m %s\n", i, line);
-		free(line);
-		line = NULL;
-	}
-	free(line);
-	i = get_next_line(fd2, &line);
-	printf("[%d]\033[32m %s\n", i, line);
-
-	// i = get_next_line(fd, &line);
-	// printf("[%d]\033[32m %s\n", i, line);
-	// free(line);
-// 	// free(line);
-// 	// printf("%s\n", ft_strjoin("", "\n"));
-}
-
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,14 +6,14 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/03 16:42:21 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/10 03:30:29 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/11 21:42:44 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t		ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	int		count;
 
@@ -69,65 +69,4 @@ char	*ft_strchr(const char *s, int c)
 	if (s[count] == c)
 		return (char *)(s + count);
 	return (0);
-}
-
-char	*ft_strdup(const char *s)
-{
-	int		count;
-	char	*tab;
-
-	count = 0;
-	while (s[count])
-		count++;
-	if (!(tab = (char *)malloc((count + 1) * sizeof(char))))
-		return (NULL);
-	count = -1;
-	while (s[++count])
-		tab[count] = s[count];
-	tab[count] = '\0';
-	return (tab);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	count;
-	size_t	size;
-	char	*tab;
-
-	count = 0;
-	if (!s)
-		return (NULL);
-	if (ft_strlen(s) < start)
-		return (ft_strdup(""));
-	size = ft_strlen(s + start);
-	if (size < len)
-		len = size;
-	if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
-		return (NULL);
-	while (count < len)
-	{
-		tab[count] = s[start + count];
-		count++;
-	}
-	tab[count] = '\0';
-	return (tab);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	size_t	count;
-	size_t	s1_size;
-	char	*tab;
-
-	count = -1;
-	s1_size = ft_strlen(s1);
-	if (!(tab = (char *)malloc((s1_size + ft_strlen(s2) + 1) * sizeof(char))))
-		return (NULL);
-	while (s1 && s1[++count])
-		tab[count] = s1[count];
-	count = -1;
-	while (s2 && s2[++count])
-		tab[s1_size + count] = s2[count];
-	tab[s1_size + count] = '\0';
-	return (tab);
 }
